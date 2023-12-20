4859a1ed94366bdaa2498fb51dce3de539e32dc0
elynGeby5 <elyngeby@gmail.com>
2023-12-21 00:20:31 +0700

    manage jadwal edit  n hpus

--- app/Http/Controllers/AdminJadwalController.php
+++ app/Http/Controllers/AdminJadwalController.php
@@ -44,21 +44,67 @@ class AdminJadwalController extends Controller
         return redirect()->route('manage-jadwal')->with('success', 'Jadwal berhasil ditambahkan.');
     }
 
-    public function addToSpecificKelas(Request $request, $id)
+    // public function addToSpecificKelas(Request $request, $id)
+    // {
+    //     $jadwal = Jadwal::find($id);
+    //     if (!$jadwal) {
+    //         return redirect()->back();
+    //     }
+
+    //     $data = $request->all();
+    //     $kelas = Kelas::find($data['id_kelas']);
+    //     $users = $kelas->users;
+
+    //     foreach ($users as $user) {
+    //         $jadwal->users()->attach($user->nis);
+    //     }
+
+    //     return redirect()->route('manage-jadwal');
+    // }
+
+    public function edit($id_jadwal)
     {
-        $jadwal = Jadwal::find($id);
-        if (!$jadwal) {
-            return redirect()->back();
-        }
+        $jadwal = Jadwal::findOrFail($id_jadwal);
+        $kelas = Kelas::all();
+        $guru = Guru::all();
+        $mapel = MataPelajaran::all();
 
-        $data = $request->all();
-        $kelas = Kelas::find($data['id_kelas']);
-        $users = $kelas->users;
-
-        foreach ($users as $user) {
-            $jadwal->users()->attach($user->nis);
-        }
-
-        return redirect()->route('manage-jadwal');
+        return view('manage-jadwal', compact('jadwal', 'kelas', 'guru', 'mapel'));
     }
+
+    public function update(Request $request, $id_jadwal)
+    {
+        $request->validate([
+            // Sesuaikan dengan kebutuhan validasi Anda
+            'kode_mapel_mapel' => 'required',
+            'nuptk_guru_guru' => 'required',
+            'hari' => 'required',
+            'jam_pelajaran' => 'required',
+            'id_kelas' => 'required',
+        ]);
+
+        $jadwal = Jadwal::findOrFail($id_jadwal);
+
+        $jadwal->update([
+            'kode_mapel_mapel' => $request->input('kode_mapel_mapel'),
+            'nuptk_guru_guru' => $request->input('nuptk_guru_guru'),
+            'hari' => $request->input('hari'),
+            'jam_pelajaran' => $request->input('jam_pelajaran'),
+            'id_kelas' => $request->input('id_kelas'),
+        ]);
+
+        return redirect()->route('manage-jadwal')->with('success', 'Jadwal berhasil diperbarui.');
+    }
+
+    public function destroy($id_jadwal)
+    {
+        $jadwal = Jadwal::findOrFail($id_jadwal);
+
+        // Lakukan logika penghapusan sesuai kebutuhan Anda
+        $jadwal->delete();
+
+        return redirect()->route('manage-jadwal')->with('success', 'Jadwal berhasil dihapus.');
+    }
+
 }
+
--- resources/views/manage-jadwal.blade.php
+++ resources/views/manage-jadwal.blade.php
@@ -150,7 +150,6 @@
                                 data-bs-target="#exampleModal">
                                 <i class="fas fa-circle-plus"></i> Tambah
                             </button>
-                            <button class="btn btn-primary rounded-1"><i class="fas fa-file-lines"></i> Laporan</button>
                         </div>
                         <div class="data-control-show-select gap-2">
                             <h5>Filter Data</h5>
@@ -219,10 +218,12 @@
                                             <td class="text-center">{{ $item['hari'] }}</td>
                                             <td class="text-center">{{ $item['jam_pelajaran'] }}</td>
                                             <td class="text-center">
-                                                <a href="#" class="btn btn-danger btn-sm"><i
-                                                        class="fas fa-edit"></i></a>
-                                                <a href="#" class="btn btn-success btn-sm"><i
-                                                        class="fas fa-trash"></i></a>
+                                                <a href="#" class="btn btn-danger btn-sm">
+                                                    <i class="fas fa-edit" data-bs-toggle="modal" data-bs-target="#editModal{{ $item['id_jadwal'] }}"></i>
+                                                </a>
+                                                <a href="#" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal{{ $item['id_jadwal'] }}" data-itemid="{{ $item['id_jadwal'] }}">
+                                                    <i class="fas fa-trash"></i>
+                                                </a>
                                             </td>
                                         </tr>
                                     @empty
@@ -230,6 +231,98 @@
                                             <td colspan="7" class="text-center">Tidak ada data</td>
                                         </tr>
                                     @endforelse
+
+                                    @foreach ($jadwal as $item)
+                                    <!-- Modal Edit -->
+                                    <div class="modal fade" id="editModal{{ $item['id_jadwal'] }}" tabindex="-1" aria-labelledby="editModalLabel"
+                                        aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <form action="{{ route('update-jadwal', $item['id_jadwal']) }}" method="POST">
+                                                    @csrf
+                                                    @method('PUT')
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="editModalLabel">Edit Data Jadwal</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <!-- Isi formulir dengan data yang akan diupdate -->
+                                                        <div class="mb-3">
+                                                            <label class="form-label">Mata Pelajaran</label>
+                                                            <select class="form-control" name="kode_mapel_mapel">
+                                                                @foreach ($mapel as $m)
+                                                                    <option value="{{ $m['kode_mapel'] }}"
+                                                                        {{ $m['kode_mapel'] == $item['mapel']['kode_mapel'] ? 'selected' : '' }}>
+                                                                        {{ $m['nama_mapel'] }}
+                                                                    </option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="form-label">Guru</label>
+                                                            <select class="form-control" name="nuptk_guru_guru">
+                                                                @foreach ($guru as $g)
+                                                                    <option value="{{ $g['nuptk_guru'] }}"
+                                                                        {{ $g['nuptk_guru'] == $item['guru']['nuptk_guru'] ? 'selected' : '' }}>
+                                                                        {{ $g['nama_guru'] }}
+                                                                    </option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="form-label">Kelas</label>
+                                                            <select class="form-control" name="id_kelas">
+                                                                @foreach ($kelas as $k)
+                                                                    <option value="{{ $k['id'] }}"
+                                                                        {{ $k['id'] == $item['kelas']['id'] ? 'selected' : '' }}>
+                                                                        {{ $k['nama_kelas'] }}
+                                                                    </option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="form-label">Hari</label>
+                                                            <input type="text" class="form-control shadow-input" name="hari" value="{{ $item['hari'] }}"
+                                                                required>
+                                                        </div>
+                                                        <div class="mb-3">
+                                                            <label class="form-label">Jam Pelajaran</label>
+                                                            <input type="number" class="form-control shadow-input" name="jam_pelajaran"
+                                                                value="{{ $item['jam_pelajaran'] }}" required>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button type="submit" class="btn btn-success">Update</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="modal fade" id="confirmDeleteModal{{ $item['id_jadwal'] }}" tabindex="-1" aria-labelledby="confirmDeleteModalLabel{{ $item['id_jadwal'] }}" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="confirmDeleteModalLabel{{ $item['id_jadwal'] }}">Konfirmasi Hapus</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Apakah anda yakin ingin menghapus data ini?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <!-- Change the form action to include the item ID -->
+                                                    <form method="POST" action="{{ route('delete-jadwal', ['id_jadwal' => $item['id_jadwal']]) }}">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+                                                        <button type="submit" class="btn btn-danger">Ya</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
                                 </tbody>
                             </table>
                             <div class="mt-5">
